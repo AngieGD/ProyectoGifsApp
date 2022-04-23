2ba1c138f79611c416666bfd85966515d5235a2f
Angie Loreina García Delgado <dev05@aiatic.com>
2022-04-23 14:59:46 -0500

    Aplicacíon modularizada y con Viewchild

---
+++ gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from "@angular/core"; //Para que sea un componente
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+    
+
+}
