6a70139453646afd3b5ede3240321aa5f46d7fcd
Angie Loreina García Delgado <dev05@aiatic.com>
2022-04-26 20:38:00 -0500

    Gifs app lista

--- gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,6 +8,13 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   ]
 })
 export class BusquedaComponent {
+
+
+  constructor(private buscarService:GifsService){
+
+
+  }
+
   //El tipo de ElementRef es porque de por si es generico, si pongo esto me va a permitir ver las propiedades del input
   @ViewChild('txtBuscar') txtBus!:ElementRef<HTMLInputElement>; //txtBus lo defino como ElementRef y el signo ! indica que le garantizo a typescritp que la propiedad va a existir
   /**
@@ -16,8 +24,20 @@ export class BusquedaComponent {
   buscar(){
 
     const valor = this.txtBus.nativeElement.value
-    console.log(valor)
+    //console.log(valor)
+    
+    if(valor.trim().length===0 ){
+      
+      return;
+
+    }else{
+      
+      this.buscarService.buscargifs(valor)
+    }
+    
     //lo siguiente es para que se borre luego de dar enter
     this.txtBus.nativeElement.value = ''
   }
+
+  
 }
--- gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,27 @@
 
 import { Component } from "@angular/core"; //Para que sea un componente
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
-    
 
+    constructor(private gifService : GifsService){}
+    
+    get historial(){
+        
+        const valor:string[] = this.gifService.historial
+        
+        return valor
+        
+    }
+
+    buscar( termino:string ){
+        console.log(termino)
+        this.gifService.buscargifs(termino)
+
+    }
+    //historiala:string[]  = this.historial()
 }
